895a72d6fd466ac7d9ee0a975410d69bc1e650e1
Scytale <federicoboainain@gmail.com>
2022-10-28 13:43:39 -0300

    commit to merge with main-container branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './component/menu/menu.component';
 import { SideBarComponent } from './component/side-bar/side-bar.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NavBarComponent } from './component/nav-bar/nav-bar.component';
+import { MainComponent } from './component/main/main.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    SideBarComponent
+    SideBarComponent,
+    NavBarComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
